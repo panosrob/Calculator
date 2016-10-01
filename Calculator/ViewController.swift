3f8ba5beb51a37f18ff25729483502c723894615
Panagiotis Rompolas <panosrob@Tyrael.local>
2016-10-01 15:08:31 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Panagiotis Rompolas on 01/10/2016.
+//  Copyright © 2016 Panagiotis Rompolas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
